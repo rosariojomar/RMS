335266ad4f296f4f5c85f915c33bdbe90f8d5b3d
rosariojomar <rosariojomar.it@gmail.com>
2024-09-10 01:32:07 +0800

    Migration and Database Backup Person Additional Fields

---
+++ RMS_DAL/Migrations/20240909172916_PeopleTableAdditionalFields.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMS_DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class PeopleTableAdditionalFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateHired",
+                table: "People",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeContractStatus",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeLocation",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeProjects",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeSkills",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeStatus",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmployeeTitle",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndOfEmployment",
+                table: "People",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "FJLevel",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Grade_Level",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBench",
+                table: "People",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JLPTLevel",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PersonTitle",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegularizationDate",
+                table: "People",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResignationDate",
+                table: "People",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZinzaiId",
+                table: "People",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateHired",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeContractStatus",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeLocation",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeProjects",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeSkills",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeStatus",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeTitle",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EndOfEmployment",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "FJLevel",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Grade_Level",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "IsBench",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "JLPTLevel",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "PersonTitle",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "RegularizationDate",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "ResignationDate",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "ZinzaiId",
+                table: "People");
+        }
+    }
+}
--- RMS_DAL/Models/Person.cs
+++ RMS_DAL/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,31 @@ namespace RMS_DAL.Models
 {
     public class Person : BaseModel
     {
+        [Key]
         public int PersonId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? Middlename { get; set; }
         public string Position { get; set; }
+        public string ZinzaiId { get; set; }
+        //Mr., Ms., Mrs.
+        public string PersonTitle { get; set; }
+        public string EmployeeStatus { get; set; }
+        public string EmployeeTitle { get; set; }
+        public string EmployeeLocation { get; set; }
+        public string Grade_Level { get; set; }
+        public string FJLevel { get; set; }
+        public string JLPTLevel { get; set; }
+        //Probationary, Regular
+        public string EmployeeContractStatus { get; set; }
+        public DateTime DateHired { get; set; }
+        public DateTime RegularizationDate { get; set; }
+        public string? EmployeeProjects { get; set; }
+        public string? EmployeeSkills { get; set; }
+        public string? Gender { get; set; }
+        public DateTime EndOfEmployment { get; set; }
+        public DateTime ResignationDate { get; set; }
+        public bool IsBench { get; set; }
         public int RBUId { get; set; }
         public int DepartmentId { get; set; }
         public int DivisionId { get; set; }
